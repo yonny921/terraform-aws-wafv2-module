ea9d2b9b41cc5d9609418961d463f1ceb1fffba8
Yonny Arias <yonny921@gmail.com>
2025-12-02 22:20:20 -0500

    Delete outputs.tf

--- modules/waf-acl/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_acl_arns" {
-  description = "Mapa de Nombres a ARNs de los WAFs creados"
-  value       = { for k, v in aws_wafv2_web_acl.this : k => v.arn }
-}
-
-output "arn_map" {
-  description = "Mapa de Nombres de IP Set a sus ARNs"
-  value       = { for k, v in aws_wafv2_ip_set.this : k => v.arn }
-}
